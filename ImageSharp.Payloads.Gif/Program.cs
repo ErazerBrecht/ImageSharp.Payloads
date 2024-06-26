6c462c83f247cb132b659c25baa8721d51a0ac25
Brecht Carlier <brecht.carlier@outlook.com>
2024-06-26 14:16:19 +0200

    Created repro's of 'malicious' payloads

---
+++ ImageSharp.Payloads.Gif/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Memory;
+
+Console.WriteLine("Hello, World!");
+
+Configuration.Default.MemoryAllocator = MemoryAllocator.Create(new MemoryAllocatorOptions
+{
+    MaximumPoolSizeMegabytes = 1024
+});
+
+await Task.WhenAll(Enumerable.Range(0, 5).Select(_ => SaveLoadImage()));
+
+return;
+
+static Task SaveLoadImage()
+{
+    return Task.Run(() =>
+    {
+        using var ms = new MemoryStream(File.ReadAllBytes("funnyanim.gif"));
+
+        var image = Image.Load(ms);
+        image.Metadata.ExifProfile = null;
+        image.Metadata.IccProfile = null;
+        image.Metadata.IptcProfile = null;
+        image.Metadata.XmpProfile = null;
+
+        var id = Guid.NewGuid();
+        image.Save($"result-{id}.gif");
+    });
+}
---
+++ ImageSharp.Payloads.Jpeg/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Memory;
+
+Console.WriteLine("Hello, World!");
+
+Configuration.Default.MemoryAllocator = MemoryAllocator.Create(new MemoryAllocatorOptions
+{
+    MaximumPoolSizeMegabytes = 1024
+});
+
+SaveLoadImage("83");
+SaveLoadImage("92");
+SaveLoadImage("93");
+
+return;
+
+static void SaveLoadImage(string fileName)
+{
+    using var input = new MemoryStream(File.ReadAllBytes($"{fileName}.jpg"));
+
+    var image = Image.Load(input);
+    image.Metadata.ExifProfile = null;
+    image.Metadata.IccProfile = null;
+    image.Metadata.IptcProfile = null;
+    image.Metadata.XmpProfile = null;
+
+    using var output = new MemoryStream();
+    var encoder = new JpegEncoder { Quality = 75 };
+    image.Save(output, encoder);
+    Console.WriteLine($"Input: {input.Length} vs Output: {output.Length}");
+}
